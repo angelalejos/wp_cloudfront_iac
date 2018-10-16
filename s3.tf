511088ff57e6cd7025d73ecfb3d616b73aa67a18
Joel Knight <knight.joel@gmail.com>
2018-10-15 21:26:45 -0600

    Lifecycle policy on the S3 logging bucket

--- s3.tf
+++ s3.tf
@@ -7,5 +7,16 @@ resource "aws_s3_bucket" "cf_logs" {
 	bucket = "${var.cf_logging_bucket_name}"
 	acl = "private"
 
+	lifecycle_rule {
+		id = "log_rotate"
+		enabled = true
+
+		prefix = "${var.cf_logging_bucket_prefix}"
+
+		expiration {
+			days = 30
+		}
+	}
+
 	tags = "${var.tags}"
 }
